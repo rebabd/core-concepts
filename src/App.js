a4e5d91867d793a6e9f7e85537ed303545474352
shorifulislambd <61221795+shorifulislambd@users.noreply.github.com>
2020-04-01 11:09:47 -0700

    core-conceps-done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Shop from './Components/Shop/Shop';
@@ -12,35 +12,45 @@ import Review from './Components/Review/Review';
 import Inventory from './Components/Inventory/Inventory';
 import Notfound from './Components/Notfound/Notfound';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
+import Login from './Components/Login/Login';
+import { AuthContextProvider, PrivateRoute } from './Components/Login/useAuth';
+import Shipment from './Components/Shipment/Shipment';
 
-function App() {
+
+function App(props) {
   return (
     <div>
-      <Header></Header>
-      <Router>
-        <Switch>
-          <Route path="/shop">
-            <Shop></Shop>
-          </Route>
-          <Route path="/review">
-           <Review></Review>
-          </Route>
-          <Route path="/inventory">
-            <Inventory></Inventory>
-          </Route>
-          <Route exact path="/">
-            <Shop></Shop>
-          </Route>
-          <Route path="/product/:productKey">
-            <ProductDetail></ProductDetail>
-          </Route>
-          <Route path="*">
-            <Notfound></Notfound>
-          </Route>
-        </Switch>
-      </Router>
-      
- 
+      <AuthContextProvider> 
+        <Header></Header>
+        <Router>
+          <Switch>
+            <Route path="/shop">
+              <Shop></Shop>
+            </Route>
+            <Route path="/review">
+            <Review></Review>
+            </Route>
+            <Route path="/inventory">
+              <Inventory></Inventory>
+            </Route>
+            <Route exact path="/">
+              <Shop></Shop>
+            </Route>
+            <Route path="/product/:productKey">
+              <ProductDetail></ProductDetail>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <PrivateRoute path="/shipment">
+              <Shipment></Shipment>
+            </PrivateRoute>
+            <Route path="*">
+              <Notfound></Notfound>
+            </Route>
+          </Switch>
+        </Router>
+        </AuthContextProvider>  
     </div>
   );
 }
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-
 const Cart = (props) => {
     const cart = props.cart;
-    //console.log(cart); 
   //  const total = cart.reduce( (total, prd) => total + prd.price, 0);
   let total = 0;
   for(let i = 0; i < cart.length; i++){
